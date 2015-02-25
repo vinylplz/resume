c0a561570c47f6af9cef2af59c2880191759f7d5
vinylplz <greg.forties@gmail.com>
2015-02-25 08:53:03 -0600

    rewrote build task to support dynamically adding output formats through the all_build_formats, default_build_formats variables and _convert_to_fmt() funtion

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -4,31 +4,50 @@ fabfile for generating the resume in multiple formats using pandoc
 
 from __future__ import with_statement
 
+import os
+import sys
+
 from fabric.api import abort, local, task, hide, puts
-from string import Template
+#from string import Template
 
 from contextlib import contextmanager
 
+resume_name = 'resume.md'
+project_root = os.path.normpath(os.path.join(__file__, os.path.pardir,
+               os.path.pardir))
+build_dir = os.path.join(project_root, 'formats')
+
+all_build_formats = ['docx', 'html', 'html5', 'pdf']
+default_build_formats = all_build_formats
 
 @task
-def build(type='all'):
+def build(type='default'):
     """
-    :$type - convert resume to file $type (default 'all').  Valid values: docx, all
+    :$type - convert resume to file $type. Ex. fab build:docx 
     """
-    if type in ['docx','all']:
-        build_docx()
+    if type == 'all':
+        formats = all_build_formats
+    elif type == 'default':
+        formats = default_build_formats
+    elif type not in all_build_formats:
+        puts("Unsupported format: '{}'".format(type))
+        puts("Default formats:   {}".format(default_build_formats))
+        puts("Supported formats: {}".format(all_build_formats))
+        sys.exit(1)
+    else:
+        formats = [type]
+
+    for fmt in formats:
+        _convert_to_fmt(fmt)
 
     puts("Finished building.")
    
-
-def build_docx():
-    """
-    Build a docx document from input file
-    """
-    with msg("Building docx"):
-        local('pandoc resume.md -o formats/resume.docx')
-
-
+def _convert_to_fmt(output_fmt):
+    input_file = os.path.join(project_root, resume_name)
+    output_file = os.path.join(build_dir, resume_name.split('.')[0] + '.' +
+                  output_fmt)
+    with msg("Building {}".format(output_fmt)):
+         local('pandoc {} -o {}'.format(input_file, output_file))
 
 
 @contextmanager
